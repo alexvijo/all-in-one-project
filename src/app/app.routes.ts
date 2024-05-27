185b49552d54866217d0d3b204b4035bfed92448
alexvijo <alexvicentejose@gmail.com>
2024-05-27 09:27:08 +0200

    Included Material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,10 @@ export const routes: Routes = [
     loadChildren: () => import('./articles/articles.routes').then(m => m.ARTICLES_ROUTER)
   },
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTER)
+  },
+  {
     path: '**',
     redirectTo: 'articles'
   }
--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    MatMenuModule,
+    MatIconModule]
+  ,
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
